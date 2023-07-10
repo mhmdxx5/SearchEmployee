1eeb47b400a528c3e1064021a809371aad76ed98
Muhammed Otmani <MuhammedO@FEMI.COM>
2023-07-11 01:58:39 +0300

    update url

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 connectDB();
 
 //Routes
-app.use("https://64ac614a931d8900814d50c7--resonant-cat-d28ea4.netlify.app/api/employees", employeeRoutes);
+app.use("/api/employees", employeeRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ "message": "Bad Method Request!" });
