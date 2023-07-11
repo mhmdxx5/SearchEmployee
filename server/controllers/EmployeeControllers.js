07419c32eb564cbcf0ac49688d9d82f70c2b4b71
Muhammed Otmani <MuhammedO@FEMI.COM>
2023-07-12 01:00:39 +0300

    dropdown_update+caching

--- server/controllers/EmployeeControllers.js
+++ server/controllers/EmployeeControllers.js
@@ -1,7 +1,7 @@
 import Employee from "../models/EmployeeModel.js";
 import { infoLogger, errorLogger } from "../logger/logger.js";
 
-
+// Add a new employee
 const AddEmployee = async (req, res, next) => {
   try {
     const { id, name, photo, role } = req.body;
@@ -12,44 +12,56 @@ const AddEmployee = async (req, res, next) => {
       photo,
       role,
     });
+
+    // Save the new employee to the database
     await newEmployee.save();
     infoLogger.info(`Employee added successfully`);
     return res.json({ message: "Employee added successfully" });
   } catch (error) {
-    errorLogger.error(`Failed to add employee ${error}.`);
-    return res.status(500).json({ "message":`Failed to add employee ${error}.`});
+    // Log and handle errors
+    errorLogger.error(`Failed to add employee: ${error}`);
+    return res.status(500).json({ "message": `Failed to add employee: ${error}` });
   }
 };
 
+// Get all employees
 const getALLEmployees = async (req, res, next) => {
   try {
+    // Retrieve all employees from the database and sort them by name
     const employees = await Employee.find().sort({ name: 1 });
-    infoLogger.info(`employees has been fetched successfully`);
+    infoLogger.info(`Employees have been fetched successfully`);
     return res.json(employees);
   } catch (error) {
-    errorLogger.error(`Failed to fetch employees ${error}.`);
-    return res.status(500).json({ "message": `Failed to fetch employees ${error}.`});
+    // Log and handle errors
+    errorLogger.error(`Failed to fetch employees: ${error}`);
+    return res.status(500).json({ "message": `Failed to fetch employees: ${error}` });
   }
 };
 
+// Get a specific employee by ID
 const getEmployee = async (req, res, next) => {
   try {
+    // Find the employee by ID
     const employee = await Employee.findById(req.params.id);
     if (!employee) {
+      // If employee is not found, return a 404 status code
       errorLogger.error(`Employee ${req.params.id} not found.`);
       return res.status(404).json({ "message": `Employee ${req.params.id} not found.` });
     }
-    infoLogger.info(`employee ${req.params.id} has been fetched successfully`);
+    infoLogger.info(`Employee ${req.params.id} has been fetched successfully`);
     return res.json(employee);
   } catch (error) {
-    errorLogger.error(`Failed to fetch employee ${req.params.id}.`);
-    return res.status(500).json({ "message": `Failed to fetch employee ${req.params.id}.` });
+    // Log and handle errors
+    errorLogger.error(`Failed to fetch employee ${req.params.id}: ${error}`);
+    return res.status(500).json({ "message": `Failed to fetch employee ${req.params.id}: ${error}` });
   }
 };
 
+// Update an employee
 const updateEmployee = async (req, res, next) => {
   const { id, name, photo, role } = req.body;
   try {
+    // Find and update the employee by ID
     const updatedEmployee = await Employee.findByIdAndUpdate(req.params.id, {
       id,
       name,
@@ -58,29 +70,35 @@ const updateEmployee = async (req, res, next) => {
     }, { new: true });
 
     if (!updatedEmployee) {
+      // If employee is not found, return a 404 status code
       errorLogger.error(`Employee ${req.params.id} not found.`);
       return res.status(404).json({ "message": `Employee ${req.params.id} not found.` });
     }
-    infoLogger.info(`employee ${req.params.id} updated successfully`);
-    return res.status(200).json({ "message": `employee ${req.params.id} updated successfully` });
+    infoLogger.info(`Employee ${req.params.id} updated successfully`);
+    return res.status(200).json({ "message": `Employee ${req.params.id} updated successfully` });
   } catch (error) {
-    errorLogger.error(`Failed to update employee:${req.params.id}.`);
-    return res.status(500).json({ "message": `Failed to update employee:${req.params.id}.`});
+    // Log and handle errors
+    errorLogger.error(`Failed to update employee ${req.params.id}: ${error}`);
+    return res.status(500).json({ "message": `Failed to update employee ${req.params.id}: ${error}` });
   }
 };
 
+// Delete an employee
 const deleteEmployee = async (req, res, next) => {
   try {
+    // Find and delete the employee by ID
     const deletedEmployee = await Employee.findByIdAndDelete(req.params.id);
     if (!deletedEmployee) {
+      // If employee is not found, return a 404 status code
       errorLogger.error(`Employee ${req.params.id} not found.`);
       return res.status(404).json({ "message": `Employee ${req.params.id} not found.` });
     }
-    infoLogger.info(`employee ${req.params.id} deleted successfully`);
+    infoLogger.info(`Employee ${req.params.id} deleted successfully`);
     return res.json({ message: `Employee deleted successfully` });
   } catch (error) {
-    errorLogger.error(`Failed to delete employee:${req.params.id}.`);
-    return res.status(500).json({ "message": `Failed to delete employee:${req.params.id}.`});
+    // Log and handle errors
+    errorLogger.error(`Failed to delete employee ${req.params.id}: ${error}`);
+    return res.status(500).json({ "message": `Failed to delete employee ${req.params.id}: ${error}` });
   }
 };
 
